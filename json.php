0be6aee929ebb319b5281ab81658aab1baa02925
Felix Mallek <git@fellek.net>
2022-12-19 00:58:22 +0100

    Add Strahlungswerte Summe

--- json.php
+++ json.php
@@ -22,44 +22,43 @@ else
 
 $file = file_get_contents($url);
 $json = json_decode($file, TRUE);
-/**
-echo "<table border='1' style='font-family: sans-serif;'><tr>";
+
+//echo "<table border='1' style='font-family: sans-serif;'><tr>";
 $i=0;
+$strahlung = array();
 foreach($json['days1h'] as $key => $day) {
-    echo "<tr>";
-    echo "<td>";
+  //  echo "<tr>";
+  //  echo "<td>";
     if ($i==0) {
         echo "Heute";
     } else {
         echo date ('l', time() + $i*3600);
     }
-    echo "</td>";
+ //   echo "</td>";
     foreach ($day['1h'] as $hour => $data) {
-        echo "<td width='150px'>";
-        echo $data['time'] . "<br>";
-        echo "Temperatur&nbsp;(min/max):&nbsp;" . $data['tair'] . "&nbsp;gef&uuml;hlt&nbsp;wie" . $data['tdew'] . "&nbsp;°C" . "<br>";
-        echo "Bew&ouml;lkung: " . $data['cloud'] . " h" . "<br>";
-        echo "Strahlung:    " . $data['radwm2'] . " W/m²" . "<br>";
-        echo "</td>";
+        if ($data['radwm2'] > 0 ) {
+            $strahlung[$i] += $data['radwm2'];
+        }
+   //     echo "<td width='150px'>";
+   //     echo $data['time'] . "<br>";
+   //     echo "Temperatur&nbsp;(min/max):&nbsp;" . $data['tair'] . "&nbsp;gef&uuml;hlt&nbsp;wie" . $data['tdew'] . "&nbsp;°C" . "<br>";
+   //     echo "Bew&ouml;lkung: " . $data['cloud'] . " h" . "<br>";
+   //     echo "Strahlung:    " . $data['radwm2'] . " W/m²" . "<br>";
+   //     echo "</td>";
         $i++;
     }
-    echo "</tr>";
+   // echo "</tr>";
 }
-echo "</tr></table>";
+print_r($strahlung);
+//echo "</tr></table>";
 
-unset($json['day1h']['tair']);
+/**unset($json['day1h']['tair']);
 unset($json['day1h']['tdew']);
 unset($json['day1h']['relhum']);
 unset($json['day1h']['radjcm2']);
-unset($json['day1h']['wsms']);
-unset($json['day1h']['wskmh']);
-unset($json['day1h']['wgustms']);
-unset($json['day1h']['wgustkmh']);
-unset($json['day1h']['wdir']);
-unset($json['day1h']['wdir_labels']);
 unset($json['day1h']['precprob']);
 unset($json['day1h']['prec']);
 //unset($json['day1h']);
 
 */
-echo json_encode($json);
+//echo json_encode($json);
